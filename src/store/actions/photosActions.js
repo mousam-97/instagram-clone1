cad3932271afd4cbfa033e4ba7596755bcd84aa3
Mousam <mousam.kalita777@gmail.com>
2022-04-01 16:59:18 +0530

    update

--- src/store/actions/photosActions.js
+++ src/store/actions/photosActions.js
@@ -1,8 +1,3 @@
-/* 
-    This is a blank file where you can start writing your
-    actions. You can feel free to add multiple actions,
-    based on your discretion.
-*/
 
 import data from "../../data";
 
